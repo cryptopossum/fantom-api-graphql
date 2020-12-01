46274bf3ecf85b9dc920a7022b39b00ed0f982e4
Jiri Malek <jirka.malek@gmail.com>
2020-12-01 14:02:26 +0100

    Update resolver for Solidity release versions API point.

--- internal/graphql/resolvers/solidity.go
+++ internal/graphql/resolvers/solidity.go
@@ -1,10 +1,10 @@
 // Package resolvers implements GraphQL resolvers to incoming API requests.
 package resolvers
 
-import "fantom-api-graphql/internal/solidity"
+import "fantom-api-graphql/internal/validator"
 
 // SolidityVersions resolves a list of Solidity releases supported
 // for smart contract validation.
 func (rs *rootResolver) SolidityVersions() []string {
-	return solidity.SolidityReleases[:]
+	return validator.SolidityReleases[:]
 }
